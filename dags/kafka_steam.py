5f66d45c315985cd0143420acce35576c15e84b8
erfanbyt <89116723+erfanbyt@users.noreply.github.com>
2025-01-14 22:20:29 -0800

    added the docker compose and the services for kafka

---
+++ dags/kafka_steam.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+import requests
+import uuid
+
+default_dag = {
+    'owner': 'airscholar',
+    'start_date': datetime(2025, 1, 1, 12, 00),
+}
+
+
+def get_data():
+    res = requests.get("https://randomuser.me/api/")
+    res = res.json()
+    res = res['results'][0]
+    return res
+
+def format_data(res):
+    data = {}
+    location = res['location']
+    data['id'] = uuid.uuid4()
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
+    return data
+
+
+res = get_data()
+formated_data = format_data(res)
+print(formated_data)
